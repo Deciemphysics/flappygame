1f5fe3fd943e975cbe9d8446e299334ab972c6cd
ronaldmarx <deciem.physics@gmail.com>
2017-04-12 10:41:23 -0600

    More came stuff

--- practice.js
+++ practice.js
@@ -1,6 +1,8 @@
 var currentState,
     width,
-    height;
+    height,
+    frames = 0,
+    hero;
 
 var states = {
     splash: 0,
@@ -9,14 +11,54 @@ var states = {
 }
 var canvas;
 var renderingContext;
+
+class Protagonist{
+    constructor(){
+        this.x = 140;
+        this.y = 0;
+        
+        this.frame = 0;
+        this.velocity = 0;
+        this.animation = [0,1,2,1];
+
+        this.rotation = 0;
+        this.radius = 12;
+
+        this.gravity = 0.25;
+        this._jump = 4.6;
+        this.peanutButter = null;
+
+        this.update = function(){
+            var h = currentState === states.splash ? 10 : 5;
+            this.frame += frames % h === 0 ? 1: 0;
+            this.frame %= this.animation.length;
+            
+        }
+
+        this.draw = function(renderingContext){
+            renderingContext.save();
+
+            renderingContext.translate(this.x, this.y);
+            renderingContext.rotate(this.rotation);
+
+            var h = this.animation[this.frame];
+            link[h].draw(renderingContext, 20, 100);
+
+            renderingContext.restore();
+        }
+    }
+}
+
 function main(){
     windowSetup();
     canvasSetup();
     currentState = states.splash;
     document.body.appendChild(canvas);
-    
+
     loadGraphics();
+    hero = new Protagonist();
 }
+
 
 function windowSetup(){
     var windowWidth = $(window).width();
@@ -40,12 +82,29 @@ function canvasSetup(){
 
 function loadGraphics(){
     var img = new Image();
-    img.src = "resources/img/link.png";
+    img.src = "resources/img/linkSheet.png";
     img.onload = function(){
         initSprites(this);
         renderingContext.fillStyle = "#8BE4Df";
         renderingContext.fillRect(0,0, width, height);
-        link.draw(renderingContext, 100, 100)
+
+       // link.draw(renderingContext, 100, 100); // this will change
+       gameLoop();
     };
     
+}
+
+function gameLoop(){
+    update();
+    render();
+    window.requestAnimationFrame(gameLoop);
+}
+
+function update(){
+    frames ++;
+    hero.update();
+}
+function render(){
+    renderingContext.fillRect(0,0,width,height);
+    hero.draw(renderingContext);
 }
--- practicesprites.js
+++ practicesprites.js
@@ -1,16 +1,28 @@
 var link;
-function Sprite(img, x, y, width, height){
-    this.img = img;
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
-}
-Sprite.prototype.draw = function(renderingContext, x, y){
-    renderingContext.drawImage(this.img, this.x, this.y, this.width, this.height, x, y, this.width, this.height); // drawImage() is BUILT INTO javascript
+
+class Sprite {
+
+    constructor(img, x, y, width, height) {
+        this.img = img;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    draw(renderingContext, x, y) { //renderingContext is the canvas
+        renderingContext.drawImage(this.img, this.x, this.y, this.width, this.height, x, y, this.width, this.height); //drawImage() is built into JavaScript
+
+    }
 }
 
-function initSprites(img){ // THIS IS WHERE YOU MAKE A NEW CHARACTER
-    link = new Sprite(img, 0, 0, 40, 50);
 
+
+function initSprites(img) { // THIS IS WHERE YOU MAKE A NEW CHARACTER
+    // link = new Sprite(img, 90, 0, 40, 50);
+    link = [
+        new Sprite(img, 0, 0, 45, 55),
+        new Sprite(img, 45, 0, 45, 55),
+        new Sprite(img, 90, 0, 45, 55)
+    ];
 }
