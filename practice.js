52125364e665821578c628ac87596252b2e1c749
ronaldmarx <deciem.physics@gmail.com>
2017-04-22 17:33:19 -0600

    Added eneimes, and trees

--- practice.js
+++ practice.js
@@ -2,6 +2,9 @@ var currentState,
     width,
     height,
     frames = 0,
+    ogroup,
+    o2group,
+    trees,
     hero;
 
 var states = {
@@ -12,64 +15,250 @@ var states = {
 var canvas;
 var renderingContext;
 
-class Antagonist {
+class OctoGroup {
     constructor() {
-        if (windowWidth < 500) {
-            this.x = 320;
-        } else {
-            this.x = 400;
+
+        this.collection = [];
+
+        this.reset = function () {
+            this.collection = [];
         }
-        this.y = 200;
 
-        this.collisionX = this.x;
-        this.collisionY = this.y;
+        this.add = function () {
+            this.collection.push(new Octorok());
+        }
 
+        this.update = function () { // How often they come in
+            //var randomFrame = 90 + 25*(Math.floor(Math.random() * 3));
+            //console.log(randomFrame);
+            if (frames % 67 === 0) { // ADD AN OCTOROK
+                this.add();
+                //console.log(this.collection.length);
+            }
+
+            for (var i = 0, len = this.collection.length; i < len; i++) {
+                var octorok = this.collection[i];
+
+
+
+                if (i == 0) {
+                    octorok.detectCollision();
+                    console.log(octorok.width)
+                }
+                octorok.x -= 3;
+                octorok.frame += frames % 10 === 0 ? 1 : 0;
+                octorok.frame %= octorok.animation.length;
+                if (octorok.x < -octorok.width) {
+                    this.collection.splice(i, 1);
+                    i--;
+                    len--;
+                }
+
+            }
+        }
+
+        this.draw = function () {
+            for (var i = 0, len = this.collection.length; i < len; i++) {
+                var octorok = this.collection[i];
+                octorok.draw();
+            }
+        }
+    }
+}
+class OctoGroup2 {
+    constructor() {
+
+        this.collection = [];
+
+        this.reset = function () {
+            this.collection = [];
+        }
+
+        this.add = function () {
+            this.collection.push(new Octorok2());
+        }
+
+        this.update = function () { // How often they come in
+            //var randomFrame = 90 + 25*(Math.floor(Math.random() * 3));
+            //console.log(randomFrame);
+            if (frames % 157 === 0) { // ADD AN OCTOROK
+                this.add();
+            }
+
+
+            for (var i = 0, len = this.collection.length; i < len; i++) {
+                var octorok2 = this.collection[i];
+
+
+
+                if (i == 0) {
+                    octorok2.detectCollision();
+                }
+
+                octorok2.x -= 3;
+                if (octorok2.x < -octorok2.width) {
+                    this.collection.splice(i, 1);
+                    i--;
+                    len--;
+                }
+                octorok2.frame += frames % 10 === 0 ? 1 : 0;
+                octorok2.frame %= octorok2.animation.length;
+            }
+        }
+
+        this.draw = function () {
+            for (var i = 0, len = this.collection.length; i < len; i++) {
+                var octorok2 = this.collection[i];
+                octorok2.draw();
+            }
+        }
+    }
+}
+
+class Octorok {
+    constructor() {
+        this.x = 400;
+        this.y = 300; //+ Math.floor(Math.random()*50);
         this.frame = 0;
-        this.velocity = 5;
-        this.animation = [0];
-
+        this.animation = [0, 1, 2, 1];
         this.rotation = 0;
-        this.radius = 12;
+        this.width = octorokSprite[0].width;
+        this.height = octorokSprite[0].height;
 
-        // this.gravity (doesnt matter)
-        // this._jump (doesnt matter)
-        this.peanutButter = null;
+        this.detectCollision = function () {
+            if (this.x <= (hero.x + hero.width - 5) && this.x >= hero.x && (this.height + 150) <= (hero.y + hero.height)) {
+                console.log("You're dead");
+                currentState = states.Splash;
+            }
+        }
 
-        // this.jumpCount (doesnt matter)
+        this.draw = function () {
+            var h = this.animation[this.frame];
 
-        // this.jump() doesnt matter
+
+            octorokSprite[h].draw(renderingContext, this.x, this.y);
+        }
+    }
+}
+class Octorok2 {
+    constructor() {
+        this.x = 400;
+        this.y = 305; //+ Math.floor(Math.random()*50);
+        this.frame = 0;
+        this.animation = [0, 1, 2, 1];
+        this.rotation = 0;
+        this.width = octorokSprite.width;
+        this.height = octorokSprite.height;
+
+        this.detectCollision = function () {
+            if (this.x <= (hero.x + hero.width - 5) && this.x >= hero.x  ) {
+                console.log("You're dead");
+                // currentState = states.Splash;
+            }
+        }
+
+        this.draw = function () {
+            var h = this.animation[this.frame];
+
+
+            octorokSprite[h].draw(renderingContext, this.x, this.y);
+        }
+    }
+}
+class TreeGroup {
+    constructor() {
+        this.collection = [];
+
+        this.reset = function () {
+            this.collection = [];
+        }
+
+        this.add = function () {
+            switch (Math.floor(Math.random() * 3)) {
+                case 0:
+                    this.collection.push(new Tree());
+                    break;
+                case 1:
+                    this.collection.push(new Tree2());
+                    break;
+                case 2:
+                    this.collection.push(new Tree3());
+                    break;
+
+            }
+        }
+
         this.update = function () {
-            var k = currentState === states.Splash ? 10 : 5;
-            this.frame += frames % k === 0 ? 1 : 0;
-            this.frame %= this.animation.length;
-            if (currentState === states.Splash) {
-                this.updateIdleVillain();
-            } else {
-                this.updatePlayVillain();
+            if (frames % 5 === 0) {
+                this.add();
+            }
+            for (var i = 0, len = this.collection.length; i < len; i++) {
+                var tree = this.collection[i];
+
+
+                tree.x -= 2;
+                if (tree.x < -tree.width) {
+                    this.collection.splice(i, 1);
+                    i--;
+                    len--;
+                }
+
+            }
+        }
+        this.draw = function () {
+            for (var i = 0, len = this.collection.length; i < len; i++) {
+                var tree = this.collection[i];
+                tree.draw();
             }
         }
 
-        this.updateIdleVillain = function(){
+    }
+}
+class Tree {
+    constructor() {
+        this.x = 400;
+        this.y = (328 + Math.floor(Math.random() * 25));
+        this.width = 100;
 
+        this.draw = function () {
+            treeSprite.draw(renderingContext, this.x, this.y);
         }
-        this.updatePlayVillain = function(){
-            this.x -= this.velocity;
-            if (this.x = 0){
-                this.destroyVillain();
-            }
+
+    }
+}
+class Tree2 {
+    constructor() {
+        this.x = 400;
+        this.y = (328 + Math.floor(Math.random() * 25));
+        this.width = 100;
+
+        this.draw = function () {
+            treeSprite2.draw(renderingContext, this.x, this.y);
         }
-        this.destroyVillain = function(){
-            // This needs to remove the wall from my game
-            // Ask how to drop a reference to the villain. Lets go see trent
+
+    }
+}
+class Tree3 {
+    constructor() {
+        this.x = 400;
+        this.y = (328 + Math.floor(Math.random() * 25));
+        this.width = 100;
+
+        this.draw = function () {
+
+            treeSprite3.draw(renderingContext, this.x, this.y);
         }
+
     }
 }
 
 
 class Protagonist { // This is our hero constructor 
     constructor() {
-        this.x = 100;
-        this.y = 150;
+        this.x = 50;
+        this.y = 145;
+        this.width = 45;
+        this.height = 55;
 
         this.frame = 0;
         this.velocity = 0;
@@ -82,7 +271,7 @@ class Protagonist { // This is our hero constructor
         this._jump = 4.6;
         this.peanutButter = null;
 
-        this.jumpCount = 2;
+        this.jumpCount = 1;
 
         this.jump = function () {
             if (this.jumpCount > 0) {
@@ -152,6 +341,9 @@ function main() { // This is our main start function
 
     loadGraphics();
     hero = new Protagonist();
+    ogroup = new OctoGroup();
+    o2group = new OctoGroup2();
+    trees = new TreeGroup();
 }
 
 
@@ -161,10 +353,10 @@ function windowSetup() { // This is defining the window for the canvas
     console.log(windowWidth);
     if (windowWidth < 500) {
         width = 320;
-        height = 430;
+        height = 370;
     } else {
         width = 400;
-        height = 430;
+        height = 370;
         inputEvent = "mousedown";
     }
     document.addEventListener(inputEvent, onPress)
@@ -198,12 +390,24 @@ function GameLoop() { // This is our base loop for the function
     window.requestAnimationFrame(GameLoop);
 }
 
+
+
 function update() { // This is our function to keep moving him
     frames++;
+    if (currentState === states.Game) {
+        ogroup.update();
+        o2group.update();
+    }
     hero.update();
+    trees.update();
 }
 
 function render() { // This places our hero
     renderingContext.fillRect(0, 0, width, height);
+    hyrule.draw(renderingContext, 0, 0);
+    // octorokSprite.draw(renderingContext, 220, 340);
+    ogroup.draw(renderingContext);
+    o2group.draw(renderingContext);
     hero.draw(renderingContext);
+    trees.draw(renderingContext);
 }
--- practicesprites.js
+++ practicesprites.js
@@ -1,5 +1,9 @@
 var link;
-
+var hyrule;
+var octorokSprite;
+var treeSprite;
+var treeSprite2;
+var treeSprite3;
 class Sprite {
 
     constructor(img, x, y, width, height) {
@@ -20,9 +24,18 @@ class Sprite {
 
 function initSprites(img) { // THIS IS WHERE YOU MAKE A NEW CHARACTER
     // link = new Sprite(img, 90, 0, 40, 50);
+    hyrule = new Sprite(img, 0, 63, 500, 373);
     link = [
         new Sprite(img, 0, 0, 45, 55),
         new Sprite(img, 45, 0, 45, 55),
         new Sprite(img, 90, 0, 45, 55)
     ];
+    octorokSprite = [
+        new Sprite(img, 150, 0, 45, 35),
+        new Sprite(img, 195, 0, 45, 35),
+        new Sprite(img, 240, 0, 45, 35)
+    ];
+    treeSprite = new Sprite(img, 3, 450, 65, 80);
+    treeSprite2 = new Sprite(img, 65, 450, 70, 80);
+    treeSprite3 = new Sprite(img, 135, 450, 70, 80);
 }
